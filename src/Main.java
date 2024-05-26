1fa48e27b4f28f647406f4dd7770e3a509591935
Home <git@github.com:Alex0609V/holloworld.git>
2024-05-26 21:35:38 +0300

    komit in push

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        for (int i = 0; i <= 10; i++) {
-            //System.out.println("вот столько раз=" + i );
-        }
-        for (int i = 2020; i < 2070; i = i-4) {
-            //System.out.println("Высокостный год" + i);
-        }
-        int selary = 65385;
-        int total = 0;
-        for (int i = 0; i < 12; i = i++) {
-            total = total + selary;
-            System.out.println("Вся зарплата которую получил программист за год" + selary);
-        }
-        }
 
-        }
+
+    }
+}
